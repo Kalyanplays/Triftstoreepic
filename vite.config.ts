bcc61ba44a05d3aa936b9d5b9189b49444732807
kalyan mohanti <kalyancloud931@gmail.com>
2025-11-29 15:47:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base:"/triftstoreepic/",
+    base:"/Triftstoreepic/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
